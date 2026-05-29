2da090410a00c7f04fa05a620410660322e6312d
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-05-29 09:43:03 +0630

    update api address

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   // Optimized Reverse Proxy Engine
-  API_BASE: 'https://suropara.com/api/',
+  API_BASE: 'https://api.suropara.com/api/',
   MEDIA_BASE: 'https://api.suropara.com/media/receipts/',
   MAIN_DOMAIN: 'https://slopara.com/',
   BANKER_DOMAINS: ['slopanel.space', 'slofinace.website', 'slofinance.website', 'surofinance.website'],
